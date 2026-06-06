38978522983874aaba332c096ff48f6baaa9bb68
Decio Ferreira <decio.jferreira@gmail.com>
2026-06-07 00:26:39 +0100

    Add notice for full capacity of Week 2 in hockey camp

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -159,7 +159,7 @@ export default function Home() {
                 </li>
                 <li className="mt-4 flex">
                   <CalendarDateRangeIcon className="h-6 w-6 flex-none" />
-                  <span className="ml-4">Semana 2: 13 a 17 de julho</span>
+                  <span className="ml-4">Semana 2: 13 a 17 de julho (lotada)</span>
                 </li>
               </ul>
               <p className="mt-4 text-sm text-zinc-600 dark:text-zinc-400">
@@ -181,6 +181,21 @@ export default function Home() {
               <p className="mt-4 text-sm italic">
                 Nota 2: O número limite de inscritos por semana, está condicionado em função da disponibilidade dos treinadores.
               </p>
+              <div class="mt-2 rounded-md bg-yellow-50 p-4 dark:bg-yellow-500/10 dark:outline dark:outline-yellow-500/15">
+                <div class="flex">
+                  <div class="shrink-0">
+                    <svg viewBox="0 0 20 20" fill="currentColor" data-slot="icon" aria-hidden="true" class="size-5 text-yellow-400 dark:text-yellow-300">
+                      <path d="M8.485 2.495c.673-1.167 2.357-1.167 3.03 0l6.28 10.875c.673 1.167-.17 2.625-1.516 2.625H3.72c-1.347 0-2.189-1.458-1.515-2.625L8.485 2.495ZM10 5a.75.75 0 0 1 .75.75v3.5a.75.75 0 0 1-1.5 0v-3.5A.75.75 0 0 1 10 5Zm0 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z" clip-rule="evenodd" fill-rule="evenodd" />
+                    </svg>
+                  </div>
+                  <div class="ml-3">
+                    <h3 class="text-sm font-medium text-yellow-800 dark:text-yellow-100">Semana 2 Lotada</h3>
+                    <div class="mt-2 text-sm text-yellow-700 dark:text-yellow-100/80">
+                      <p>Devido à elevada procura, a Semana 2 já atingiu a sua lotação máxima. Continuam disponíveis vagas para a Semana 1.</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
               <Button target="_blank" href="https://forms.gle/p4oop7ahqiPL62dVA" variant="primary" className="group mt-6 w-full">
                 Inscreve-te!
                 <PencilSquareIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
